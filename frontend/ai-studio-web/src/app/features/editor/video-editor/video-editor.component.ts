1238b4d28304e46eb51ce236800a0826bf8a869a
JavaPeCode <javapecode@gmail.com>
2026-07-20 17:34:31 +0530

    Create feature components

---
+++ frontend/ai-studio-web/src/app/features/editor/video-editor/video-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-editor.component.html',
+  styleUrl: './video-editor.component.css'
+})
+export class VideoEditorComponent {
+
+}
--- frontend/ai-studio-web/src/app/layout/shell/shell.component.ts
+++ frontend/ai-studio-web/src/app/layout/shell/shell.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.css'
 })
